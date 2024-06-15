e9759b32cd9254d973cb390289202abdb7947abd
HudsonAdjetey <adjetey545@gmail.com>
2024-06-15 11:38:04 -0700

    data context second flatlist data

--- coffee/constants/dataContent.js
+++ coffee/constants/dataContent.js
@@ -1,6 +1,11 @@
 import Image1 from "../assets/images/image1.png";
 import Image3 from "../assets/images/image2.png";
 import Image2 from "../assets/images/image3.png";
+// Beas
+import Bean1 from "../assets/images/beans1.png";
+import Bean2 from "../assets/images/beans2.png";
+import Bean3 from "../assets/images/beans3.png";
+import Bean4 from "../assets/images/beans4.png";
 
 export const DataContent = [
   {
@@ -37,5 +42,57 @@ export const DataContent = [
     price: 4.2,
     subContent: "with foamed milk",
     img: Image1,
+  },
+];
+
+export const dataBeans = [
+  {
+    id: 1112,
+    title: "Arabica",
+    price: 4.99,
+    subContent: "High-quality beans",
+    img: Bean1,
+  },
+  {
+    id: 212,
+    title: "Robusta",
+    price: 3.99,
+    subContent: "Medium roasted",
+    img: Bean2,
+  },
+  {
+    id: 310,
+    title: "Espresso",
+    price: 4.2,
+    subContent: "with foamed milk",
+    img: Bean3,
+  },
+  {
+    id: 448,
+    title: "Macchiato",
+    price: 4.2,
+    subContent: "with foamed milk",
+    img: Bean4,
+  },
+  {
+    id: 519,
+    title: "Macchiato",
+    price: 4.2,
+    subContent: "with foamed milk",
+    img: Bean2,
+  },
+  {
+    id: 231,
+    title: "Arabica",
+    price: 4.99,
+    subContent: "High-quality beans",
+    img: Bean4,
+  },
+  {
+    id: 112,
+    title: "Robusta",
+    price: 3.99,
+    subContent: "Medium roasted",
+    img: Bean1,
   },
 ];
