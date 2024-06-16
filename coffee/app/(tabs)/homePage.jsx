f40b64beb891feb383444d99d5fd305d4123caa0
HudsonAdjetey <adjetey545@gmail.com>
2024-06-16 12:57:31 -0700

    resurface

--- coffee/app/(tabs)/homePage.jsx
+++ coffee/app/(tabs)/homePage.jsx
@@ -20,7 +20,6 @@ import { DataContent, dataBeans } from "../../constants/dataContent";
 import { Link, router, usePathname } from "expo-router";
 import Product from "../../components/Product";
 import Header from "../../components/Header";
-import ProductListing from "../../components/ProductListing";
 
 const Page = () => {
   const [activeData, setActiveData] = useState(0);
@@ -58,13 +57,112 @@ const Page = () => {
               gap: 20,
             }}
           >
-            {/* PRODUCT LISTING */}
-            <ProductListing
-              item={item}
-              width={width}
-              navigateToIndPage={() => navigateToIndPage()}
-            />
-            {/* PRODUCT LISTING */}
+            <View
+              style={{
+                alignItems: "center",
+                justifyContent: "space-between",
+              }}
+            >
+              <View
+                style={{
+                  backgroundColor: "rgba(38, 43, 51, 0.63)",
+                  flex: 1,
+                  height: 290,
+                  padding: 12,
+                  borderRadius: 10,
+                  overflow: "hidden",
+                }}
+              >
+                <TouchableOpacity
+                  onPress={() => navigateToIndPage(item)}
+                  style={{
+                    alignItems: "center",
+                    justifyContent: "center",
+                    flexDirection: "row",
+                  }}
+                >
+                  <Image
+                    style={{
+                      width: width > 450 ? 140 : "100%",
+
+                      borderRadius: 10,
+                      marginBottom: 15,
+                    }}
+                    source={item.img}
+                  />
+                </TouchableOpacity>
+                <Text
+                  style={{
+                    fontSize: 20,
+                    color: Color.white,
+                    fontWeight: "600",
+                    letterSpacing: 1,
+                  }}
+                >
+                  {item.title}
+                </Text>
+                <Text
+                  style={{
+                    fontSize: 17,
+                    letterSpacing: 1,
+                    color: Color.lightGray,
+                  }}
+                >
+                  {item.subContent}
+                </Text>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    marginTop: 15,
+                    gap: 25,
+                  }}
+                >
+                  <View
+                    style={{
+                      flexDirection: "row",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                      gap: 10,
+                    }}
+                  >
+                    <Text
+                      style={{
+                        fontSize: 23,
+                        color: Color.secondary,
+                        marginTop: 14,
+                      }}
+                    >
+                      Gh
+                    </Text>
+                    <Text
+                      style={{
+                        fontSize: 24,
+                        color: Color.white,
+                        marginTop: 14,
+                        fontWeight: "600",
+                      }}
+                    >
+                      {item.price}
+                    </Text>
+                  </View>
+                  <View
+                    style={{
+                      backgroundColor: Color.secondary,
+                      flexDirection: "row",
+                      width: 40,
+                      height: 40,
+                      justifyContent: "center",
+                      alignItems: "center",
+                      borderRadius: 10,
+                    }}
+                  >
+                    <Icon name="plus" color={"white"} size={24} />
+                  </View>
+                </View>
+              </View>
+            </View>
           </View>
         )}
         ListHeaderComponent={() => (
@@ -149,10 +247,141 @@ const Page = () => {
                 </View>
               )}
             />
-            <Product
-              DataContent={DataContent}
-              Icon={Icon}
-              navigateToIndPage={() => navigateToIndPage()}
+            <FlatList
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              style={{
+                marginTop: 20,
+                marginBottom: 20,
+              }}
+              data={DataContent}
+              key={(items) => Math.random()}
+              keyExtractor={(items) => items.id++}
+              renderItem={({ item }) => (
+                <View
+                  style={{
+                    flex: 1,
+                    // gap: 20,
+                    position: "relative",
+                    marginRight: 20,
+                  }}
+                >
+                  <Text
+                    style={{
+                      fontSize: 20,
+                      fontWeight: "600",
+                      marginRight: 10,
+                      color: Color.unactive,
+                    }}
+                  >
+                    <View
+                      style={{
+                        backgroundColor: "rgba(38, 43, 51, 0.63)",
+                        width: 200,
+                        padding: 12,
+                        borderRadius: 10,
+                        overflow: "hidden",
+                      }}
+                    >
+                      <View
+                        style={{
+                          alignItems: "center",
+                          justifyContent: "center",
+                          flexDirection: "row",
+                        }}
+                      >
+                        <TouchableOpacity
+                          onPress={() => navigateToIndPage(item)}
+                          style={{
+                            alignItems: "center",
+                            justifyContent: "center",
+                            flexDirection: "row",
+                          }}
+                        >
+                          <Image
+                            style={{
+                              width: "100%",
+                              borderRadius: 10,
+                              marginBottom: 15,
+                            }}
+                            source={item.img}
+                          />
+                        </TouchableOpacity>
+                      </View>
+                      <Text
+                        style={{
+                          fontSize: 20,
+                          color: Color.white,
+                          fontWeight: "600",
+                          letterSpacing: 1,
+                        }}
+                      >
+                        {item.title}
+                      </Text>
+                      <Text
+                        style={{
+                          fontSize: 17,
+                          letterSpacing: 1,
+                          color: Color.lightGray,
+                        }}
+                      >
+                        {item.subContent}
+                      </Text>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                          alignItems: "center",
+                          marginTop: 15,
+                          gap: 25,
+                        }}
+                      >
+                        <View
+                          style={{
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            alignItems: "center",
+                            gap: 10,
+                          }}
+                        >
+                          <Text
+                            style={{
+                              fontSize: 23,
+                              color: Color.secondary,
+                              marginTop: 14,
+                            }}
+                          >
+                            Gh
+                          </Text>
+                          <Text
+                            style={{
+                              fontSize: 24,
+                              color: Color.white,
+                              marginTop: 14,
+                              fontWeight: "600",
+                            }}
+                          >
+                            {item.price}
+                          </Text>
+                        </View>
+                        <View
+                          style={{
+                            backgroundColor: Color.secondary,
+                            flexDirection: "row",
+                            width: 40,
+                            height: 40,
+                            justifyContent: "center",
+                            alignItems: "center",
+                            borderRadius: 10,
+                          }}
+                        >
+                          <Icon name="plus" color={"white"} size={24} />
+                        </View>
+                      </View>
+                    </View>
+                  </Text>
+                </View>
+              )}
             />
 
             <Text
