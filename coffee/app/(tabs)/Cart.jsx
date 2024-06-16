fd2905ebcca20f827d072172db3c354b99552872
HudsonAdjetey <adjetey545@gmail.com>
2024-06-16 13:38:20 -0700

    adding cart intro

--- coffee/app/(tabs)/Cart.jsx
+++ coffee/app/(tabs)/Cart.jsx
@@ -1,15 +1,21 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
+import Color from "../../constants/Color";
 
 const CartPage = () => {
   return (
-    <SafeAreaView>
-      <Text>Cart</Text>
+    <SafeAreaView style={styles.container}>
+      <Text style={{ color: "#fff" }}>Cart</Text>
     </SafeAreaView>
   );
 };
 
 export default CartPage;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: Color.primary,
+  },
+});
